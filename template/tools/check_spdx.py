b443a8e5822d11fcbf25efdc6828eb534ba120a5
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2026-03-06 08:55:22 +0100

    Apply templates (#9)
    * Apply templates to itself
    
    * Apply formatting
    
    * Fi .prettierignore
    
    * Remove CI, docs, tests, and tooling
    
    * Update pixi config for template structure
    
    * Remove pixi unit-tests pre-commit hook
    
    * Add SPDX license headers to tools scripts
    
    * Remove Badgery config
    
    * Update docs for spdx-add and spdx-check
    
    * Fix formatting

--- template/tools/check_spdx.py
+++ template/tools/check_spdx.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
+# SPDX-License-Identifier: BSD-3-Clause
 """Check SPDX headers in Python files."""
 
 from __future__ import annotations
